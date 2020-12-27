c4f75497681ba606113e13c7292b51644b659fb4
arifak22 <el.shevarif@gmail.com>
2020-12-27 13:59:08 +0700

    Auth Middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -52,6 +52,9 @@ class Kernel extends HttpKernel
      */
     protected $routeMiddleware = [
         'auth' => \App\Http\Middleware\Authenticate::class,
+        'access' => \App\Http\Middleware\CheckAccess::class,
+        'jwt.verify' => \App\Http\Middleware\JwtMiddleware::class,
+        
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
         'bindings' => \Illuminate\Routing\Middleware\SubstituteBindings::class,
         'cache.headers' => \Illuminate\Http\Middleware\SetCacheHeaders::class,
